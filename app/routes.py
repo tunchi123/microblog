78516eb39e872d233a21201570f0c82bf8d30ff1
Henokmekonnen1234 <henokmac2425@gmail.com>
2023-12-21 20:55:09 +0300

    added evironmental virable

--- app/routes.py
+++ app/routes.py
@@ -9,4 +9,4 @@ from app import app
 @app.route("/")
 @app.route("/index")
 def index():
-    return "hello world"
+    return "hello world pise of shit"
---
+++ microblog.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+"""
+Main application module
+"""
+
+from app import app
